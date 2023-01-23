8fa0256de0ad4c47f7b1bfef850cb3b844888af0
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2023-01-23 08:54:44 +0000

    1

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 crear codigo actualizado con funciones y comentarios:
 
-Tienes que escribir una función de comparación que pueda volver a ordenar las líneas en su orden correcto, de lo contrario, ¡se cancelará la Navidad!
+def dots_and_boxes(links):
+    # Initialize 3x3 grid filled with zeros
+    grid = [[0 for i in range(3)] for j in range(3)]
+    # Initialize score for both players
+    player1_score = 0
+    player2_score = 0
+    
+    # Iterate through links
+    for link in links:
+        # Check if link is horizontal
+        if link[0] == link[2]:
+            # Check if boxes above and below the link are empty
+            if grid[link[0]-1][link[1]] == 0 and grid[link[0]+1][link[1]] == 0:
+                player1_score += 1
+            # Check if only one box above or below the link is empty
+            elif grid[link[0]-1][link[1]] == 0 or grid[link[0]+1][link[1]] == 0:
+                player2_score += 1
+        # Check if link is vertical
+        elif link[1] == link[3]:
+            # Check if boxes left and right of the link are empty
+            if grid[link[0]][link[1]-1] == 0 and grid[link[0]][link[1]+1] == 0:
+                player1_score += 1
+            # Check if only one box left or right of the link is empty
+            elif grid[link[0]][link[1]-1] == 0 or grid[link[0]][link[1]+1] == 0:
+                player2_score += 1
+        # Mark the link on the grid
+        grid[link[0]][link[1]] = 1
+    
+    # Return scores as tuple
+    return (player1_score, player2_score)
 
-Recordatorio: a continuación, se muestra cómo debería verse el verso final
-
-El día 12 de Navidad mi verdadero amor me dio
-
-12 bateristas tocando el tambor,
-
-tubería de 11 gaiteros,
-
-10 señores un salto,
-
-9 damas bailando,
-
-8 criadas un ordeño,
-
-7 cisnes nadando,
-
-6 gansos una puesta,
-
-5 anillos de oro,
-
-4 pájaros cantando,
-
-3 gallinas francesas,
-
-2 tórtolas y
-
-1 perdiz en un peral.
